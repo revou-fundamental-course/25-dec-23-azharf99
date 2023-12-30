bb36c1454a33f820e030225022e2eb75495dd323
azharfa99 <azharfaturohman29@gmail.com>
2023-12-30 13:24:00 +0700

    nambah fitur keliling segitiiga

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
     var alas = document.getElementById('alas')
     var tinggi = document.getElementById('tinggi')
     var result = document.getElementById('result')
 
     document.getElementById('save').addEventListener('click', (e)=>{
-        result.textContent = ""
-        result.textContent += `Alas: ${alas.value}, Tinggi: ${tinggi.value}`
-        result.textContent += `Hasil: ${(alas.value*tinggi.value)/2}`
 
-        e.preventDefault()
+        if (alas.value && tinggi.value){
+            result.textContent = ""
+            const p0 = result.appendChild(document.createElement("p"))
+            p0.textContent = "Hasil Perhitungan:"
+            const p1 = result.appendChild(document.createElement("p"))
+            p1.textContent = "L = 1/2 x a x t"
+            const p2 = result.appendChild(document.createElement("p"))
+            p2.textContent = `L = 1/2 x ${alas.value} x ${tinggi.value}`
+            const p3 = result.appendChild(document.createElement("p"))
+            p3.textContent =  `L = ${(alas.value*tinggi.value)/2} cm`
+            e.preventDefault()
+        }
+
+
     })
 
     document.getElementById('reset').addEventListener('click', (e)=>{
@@ -20,3 +30,4 @@ document.addEventListener('DOMContentLoaded', ()=>{
     })
 
 })
+
